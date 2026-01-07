064512531bac335874eecf6d0c1d6e77078948fb
Cyrus Espiritu <jcyrus28@gmail.com>
2026-01-08 01:53:54 +0700

    fix: update GhostWire formula to version 0.3.1 with correct SHA256 checksums

--- Formula/ghostwire.rb
+++ Formula/ghostwire.rb
@@ -1,16 +1,16 @@
 class Ghostwire < Formula
   desc "Secure, ephemeral TUI chat client built with Rust and Ratatui"
   homepage "https://github.com/jcyrus/GhostWire"
-  version "0.3.0"
+  version "0.3.1"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/jcyrus/GhostWire/releases/download/v#{version}/ghostwire-darwin-arm64"
-      sha256 "PUT_ARM64_SHA256_HERE" # Will be updated per release
+      sha256 "4c7bdbc6c5ab47e502a46d3f764bc889160f82ff4d73cf4dcd7f75845513ea7d"
     else
       url "https://github.com/jcyrus/GhostWire/releases/download/v#{version}/ghostwire-darwin-amd64"
-      sha256 "PUT_AMD64_SHA256_HERE" # Will be updated per release
+      sha256 "ce86ae09fcd3791027a071e8ce95d3d9681b12d52aed7ccd9958688b906ee98d"
     end
   end
 

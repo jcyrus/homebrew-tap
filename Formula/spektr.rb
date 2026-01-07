d25d878ac3f77137383be6005d48cceb75260596
Cyrus Espiritu <jcyrus28@gmail.com>
2026-01-08 01:33:53 +0700

    fix: update Spektr formula to version 0.1.1 with correct URLs and SHA256 checksums

--- Formula/spektr.rb
+++ Formula/spektr.rb
@@ -1,31 +1,39 @@
 class Spektr < Formula
   desc "Blazing-fast TUI utility for cleaning development artifacts"
   homepage "https://github.com/jcyrus/spektr"
-  version "0.1.0"
+  version "0.1.1"
   license "MIT"
+
+  head "https://github.com/jcyrus/spektr.git", branch: "main"
+
+  depends_on "rust" => :build if build.head?
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/jcyrus/spektr/releases/download/v0.1.0/spektr-v0.1.0-aarch64-apple-darwin.tar.gz"
-      sha256 "a3eefe2f53bf6ce3cdf4b350a99d4ec7e6e9488f7db98e0a28514719e93550e1"
+      url "https://github.com/jcyrus/spektr/releases/download/v0.1.1/spektr-v0.1.1-aarch64-apple-darwin.tar.gz"
+      sha256 "99f7651bafb50514f332bc5402ccdd8c836acc1be99544ed2fe050760e441381"
     else
-      url "https://github.com/jcyrus/spektr/releases/download/v0.1.0/spektr-v0.1.0-x86_64-apple-darwin.tar.gz"
-      sha256 "bdc43805fe9b8ee4fc8f5fa7a882d7f147e2460ddeb2c933407cedb422d3e77c"
+      url "https://github.com/jcyrus/spektr/releases/download/v0.1.1/spektr-v0.1.1-x86_64-apple-darwin.tar.gz"
+      sha256 "322b0aef06091f57672f27f672667e6b3c8b97fd293959d5916907882a16791a"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/jcyrus/spektr/releases/download/v0.1.0/spektr-v0.1.0-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "ea479c1981895e0843f62644457acb6fe848229190694951a7b297fd6bb09499"
+      url "https://github.com/jcyrus/spektr/releases/download/v0.1.1/spektr-v0.1.1-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "b8a73e694df464b8c6e9f040602df131757c1fcfaa6ecf3b4383a12b60021a9f"
     else
-      url "https://github.com/jcyrus/spektr/releases/download/v0.1.0/spektr-v0.1.0-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "184722a9d14a7c8969a0f3bbf204386cd72d0c861636ce31016bcf3db2e9a142"
+      url "https://github.com/jcyrus/spektr/releases/download/v0.1.1/spektr-v0.1.1-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "c679c92af49dfb66e468febae4fdec56200ecd46299b336315ab2c5c92c2fefc"
     end
   end
 
   def install
-    bin.install "spektr"
+    if build.head?
+      system "cargo", "install", *std_cargo_args
+    else
+      bin.install "spektr"
+    end
   end
 
   test do

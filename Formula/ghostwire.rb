f6d5091600e54acfa80d74adcc6db14fc77a5486
Cyrus Espiritu <jcyrus@users.noreply.github.com>
2026-03-08 14:14:26 +0700

    Update GhostWire formula to v0.4.1

--- Formula/ghostwire.rb
+++ Formula/ghostwire.rb
@@ -1,23 +1,23 @@
 class Ghostwire < Formula
   desc "Secure, ephemeral TUI chat client built with Rust and Ratatui"
   homepage "https://github.com/jcyrus/ghostwire"
-  version "0.4.0"
+  version "0.4.1"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.intel?
       url "https://github.com/jcyrus/ghostwire/releases/download/v#{version}/ghostwire-darwin-amd64"
-      sha256 "3845a8ccd2edfcf803e137817acc131fe1584e40efc3ab78532c865001c05ecb"
+      sha256 "b21c321063396bbc4c19e33b8c7da0b581a74278d76fef738e8f5c8fe71e30e1"
     elsif Hardware::CPU.arm?
       url "https://github.com/jcyrus/ghostwire/releases/download/v#{version}/ghostwire-darwin-arm64"
-      sha256 "50af615d416001a4c5a0f2aae76565b271a086d5bd6e16b54f1f3a51c7ebe125"
+      sha256 "695162aedb52fc58fa3dff93ef6108edc9c4e3b0e959647d7eb6d6652b749682"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
       url "https://github.com/jcyrus/ghostwire/releases/download/v#{version}/ghostwire-linux-amd64"
-      sha256 "2f1254f9ac22647c2bbaed7b02362e0bb2bef42aa88ea5ad6b88407bbc758984"
+      sha256 "faefd1acf8be625e0c59ac65159e7cc4fb02bf263d39557b7dad069b762b2477"
     end
   end
 

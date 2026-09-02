387fe90da67c2cd17da13de75fea19325ba85697
Cyrus Espiritu <jcyrus28@gmail.com>
2026-09-02 19:32:35 +0700

    vacate: switch to prebuilt release tarballs (v0.1.0)

--- Formula/vacate.rb
+++ Formula/vacate.rb
@@ -4,18 +4,36 @@ class Vacate < Formula
   version "0.1.0"
   license "MIT"
 
-  # jcyrus/vacate ships no release tarballs yet, so build from the crates.io
-  # source crate. The #{version} interpolation keeps update-formula.yml able to
-  # bump this in place.
-  url "https://static.crates.io/crates/vacate/vacate-#{version}.crate"
-  sha256 "63b9367285dbfc5823057bd3298658a171a4c4a4ad31cb282a10a65efe204969"
-
   head "https://github.com/jcyrus/vacate.git", branch: "main"
 
-  depends_on "rust" => :build
+  depends_on "rust" => :build if build.head?
+
+  on_macos do
+    if Hardware::CPU.arm?
+      url "https://github.com/jcyrus/vacate/releases/download/v#{version}/vacate-v#{version}-aarch64-apple-darwin.tar.gz"
+      sha256 "21921cdc4eba56efad9fddfa616c42f1aa37eb86e4e4768e8800c1ad864507b1"
+    else
+      url "https://github.com/jcyrus/vacate/releases/download/v#{version}/vacate-v#{version}-x86_64-apple-darwin.tar.gz"
+      sha256 "ded2006cea0e8e4fa5103380879b0d53ebb6e63eaac8691dfc2555d53b9fed34"
+    end
+  end
+
+  on_linux do
+    if Hardware::CPU.arm?
+      url "https://github.com/jcyrus/vacate/releases/download/v#{version}/vacate-v#{version}-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "21d41bee6f0948bc2cebba093964424068e4c5fc3d7fafc7eecbf58344f50999"
+    else
+      url "https://github.com/jcyrus/vacate/releases/download/v#{version}/vacate-v#{version}-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "a1c12a243efad1ff2ad9b267e262ab6eae74bfcc18ab4e0528559051cbb8acc1"
+    end
+  end
 
   def install
-    system "cargo", "install", *std_cargo_args
+    if build.head?
+      system "cargo", "install", *std_cargo_args
+    else
+      bin.install "vacate"
+    end
   end
 
   test do
